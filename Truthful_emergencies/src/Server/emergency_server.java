ec8e74d85d9a6aeda1fa3fe227a0206487c466b3
jgvalves <jorge_alves_93@hotmail.com>
2016-11-16 14:00:22 +0000

    Implementação do encrypt, decrypt e getKey...

--- Truthful_emergencies/src/Server/emergency_server.java
+++ Truthful_emergencies/src/Server/emergency_server.java
@@ -3,17 +3,23 @@ package Server;
 import Interface.IClient;
 import Interface.Iemergency;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.security.Key;
 import java.text.SimpleDateFormat;
+import java.util.Base64;
 import java.util.Calendar;
 import java.util.Scanner;
 import java.util.Date;
 
-
+import static javax.xml.bind.DatatypeConverter.*;
 
 
 public class emergency_server implements Iemergency
@@ -23,16 +29,20 @@ public class emergency_server implements Iemergency
     private static String type_injury;
     private static String jorge = "C:/Users/jorge/OneDrive/Documentos/Java SIRS/Truthful_emergencies/Log/Logfile.txt";
     private static String jorge2 = "C:/Users/jorge/OneDrive/Documentos/Java SIRS/Truthful_emergencies/Log/Offensefile.txt";
+    private static String jorge3 = "C:/Users/jorge/OneDrive/Documentos/Java SIRS/Truthful_emergencies/Log/Keys.txt";
     private static String leo = "C:/Users/lj0se/IdeaProjects/Truthful_emergencies/Log/Logfile.txt";
     private static String leo2 = "C:/Users/lj0se/IdeaProjects/Truthful_emergencies/Log/Offensefile.txt";
     private static int session = 1;
     private static boolean EmergencyReceived = false;
+    private static String key;
 
-    public emergency_server(){}
+    public emergency_server(){
+        generateKey();
+        writeKeyFile();
+    }
 
     public static void main(String []args)
     {
-        getSession();
         try {
 
             emergency_server obj = new emergency_server();
@@ -40,6 +50,8 @@ public class emergency_server implements Iemergency
 
             Registry registry = LocateRegistry.createRegistry(1000);
             registry.bind("Iemergency", stub);
+
+
 
             System.err.println("Server ready");
         } catch (Exception e) {
@@ -56,9 +68,10 @@ public class emergency_server implements Iemergency
     {
         int countSeenSessions = 0;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(leo))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(jorge))) {
             String line;
             while ((line = br.readLine()) != null) {
+                line = decryptString(line);
                 String[] parts = line.split(" ");
                 if(parts[1].equals("Session"))
                 {
@@ -66,7 +79,9 @@ public class emergency_server implements Iemergency
                 }
             }
         }
-        catch(Exception e){}
+        catch(Exception e){
+            System.out.println("Exception on split: " + e);
+        }
 
         session = countSeenSessions + 1;
     }
@@ -87,7 +102,7 @@ public class emergency_server implements Iemergency
     public void checkOffense(boolean bool){
         if(bool){
             try {
-                File file = new File(leo2);
+                File file = new File(jorge2);
 
                 if (!file.exists()) {
                     file.createNewFile();
@@ -108,26 +123,43 @@ public class emergency_server implements Iemergency
     //END OF REMOTE FUNCTIONS
 
     private static void createFileEntry(){
+
         try {
-            String str = getData();
-            File file = new File(leo);
+            File file = new File(jorge);
 
             if (!file.exists()) {
                 file.createNewFile();
-                FileWriter fileWriter = new FileWriter(file.getAbsoluteFile(), true);
-                BufferedWriter bw = new BufferedWriter(fileWriter);
-                bw.write(str + "\n");
-                bw.close();
             }
-            else {
-
-
+                getSession();
+                String str = getData();
                 FileWriter fileWriter = new FileWriter(file.getAbsoluteFile(), true);
                 BufferedWriter bw = new BufferedWriter(fileWriter);
 
-                bw.write(str + "\n");
+                bw.write(encryptString(str) + "\n");
                 bw.close();
+        }
+        catch(IOException e){
+            System.out.println("Error 1 on file");
+        }
+        catch(Exception e){
+            System.out.println("Error Encrypting session");
+        }
+        System.out.println("New session started: Session " + session);
+    }
+
+    private void writeKeyFile(){
+        try {
+            File file = new File(jorge3);
+
+            if (!file.exists()) {
+                file.createNewFile();
             }
+
+            FileWriter fileWriter = new FileWriter(file.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+
+            bw.write(key + "\n");
+            bw.close();
         }
         catch(IOException e){
             System.out.println("Error 1 on file");
@@ -143,6 +175,59 @@ public class emergency_server implements Iemergency
         sdf = new SimpleDateFormat("dd/MM/yyyy");
         data += sdf.format(cal.getTime());
 
+        return data;
+    }
+
+
+    private void generateKey(){
+    try {
+
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(128);
+        SecretKey skey = generator.generateKey();
+        byte[] raw = skey.getEncoded();
+        this.key = printHexBinary(raw);
+    }
+    catch(Exception e) {
+        System.out.println("Error Creating key: " + e);
+    }
+}
+
+    public static String encryptString(String str) throws Exception{
+        Cipher encryptCipher = Cipher.getInstance("AES");
+        byte[] byteKey = parseHexBinary(key);
+        SecretKeySpec skeySpec = new SecretKeySpec(byteKey, "AES");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+
+        byte[] encryptedString = encryptCipher.doFinal(str.getBytes());
+        //return printHexBinary(encryptedString);
+        return printHexBinary(encryptedString);
+    }
+
+    public static String decryptString(String str) throws Exception{
+
+        String newString = "";
+
+        Cipher encryptCipher = Cipher.getInstance("AES");
+        byte[] byteKey = parseHexBinary(key);
+        SecretKeySpec skeySpec = new SecretKeySpec(byteKey, "AES");
+        encryptCipher.init(Cipher.DECRYPT_MODE, skeySpec);
+
+        byte[] text = fromHexString(str);
+
+        byte[] decrypted = encryptCipher.doFinal(text);
+        newString = new String(decrypted, "ASCII");
+
+        return newString;
+    }
+
+    public static byte[] fromHexString(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
         return data;
     }
 
